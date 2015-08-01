6ed3cedd2d82867e79e3e264e404fe5aae75c2c0
Rishav Kundu <rishav.kundu98@gmail.com>
2015-08-01 19:30:50 +0530

    Fixed bug in gif_search.py

--- modules/gif_search.py
+++ modules/gif_search.py
@@ -4,14 +4,17 @@ def main(search_string, priv=False):
     giphy_url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag={}".format(search_string)
     try:
         j = urlopen(giphy_url).read()
-        parsed_data = json.loads(j)
+    except:
+        return 'The search failed :/'
+
+    parsed_data = json.loads(j)
+    try:
         if parsed_data['meta']['status'] == 200:
             return parsed_data['data']['image_url']
-        else:
-            raise
-    except Exception, e:
-        return 'The search failed, Jim :(' + str(e)
-        
-        
-    
-    
+    except Exception: 
+        return 'Nothing found for that :/'
+
+
+
+
+
