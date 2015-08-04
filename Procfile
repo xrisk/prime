b29fba4ccdb224fe30b8258ef774cb04110423e1
Rishav Kundu <rishav.kundu98@gmail.com>
2015-08-05 01:57:36 +0530

    Did I mention heroku sucks?

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: python server.py
-worker: python prime.py
--- server.py
+++ server.py
@@ -3,26 +3,29 @@ import sys, random, os, socket, urllib, json, urlparse, cgi
 
 
 
-mimetable = {'.html':'text/html', '.css':'text/css'}
 
-db = None
+
+
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
   
   def do_GET(self):
-      self.wfile.write('This page serves as a stupid wrapper, because Heroku has to have a web dyno running. #muchlogic')
+      print self.path
+      self.send_response(200, 'Paste Found')
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+      self.wfile.write('HELLO WORLD')
+
   
     
     
+    
 def main():
-  global db
   if 'PORT' in os.environ:
     HOST, PORT = socket.gethostname(), int(os.environ['PORT'])
   else:
     HOST, PORT = "localhost", random.choice(range(5000, 10000))
+  print HOST, PORT
   httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
-  
-  
   print "serving at port", PORT
   httpd.serve_forever()
   
-main()
