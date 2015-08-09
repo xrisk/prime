c51dc10502296ffc0826c0f1a42b4722e3b88c9d
DizzyCode <DizzyCode@users.noreply.github.com>
2015-08-09 08:31:15 -0500

    Edit prompt messages

--- credentials/get_creds.py
+++ credentials/get_creds.py
@@ -8,33 +8,31 @@ def open(file,mode='r',buffering=-1):
 def get_credentials():
     import os
     res = {}
-    
+    print "SETUP OPTIONS FOR PRIME"
     if 'ChatExchangeU' in os.environ:
         res['user'] = os.environ['ChatExchangeU']
     else: 
-        res['user'] = raw_input('Enter your Stack Overflow email ID:')
+        res['user'] = raw_input('Enter your Stack Overflow email ID: ')
     
     if 'ChatExchangeP' in os.environ:
         res['pass'] = os.environ['ChatExchangeP']
     else:
-        res['pass'] = (raw_input('Enter your Stack Overflow password:'))
+        res['pass'] = (raw_input('Enter your Stack Overflow password: '))
         
     if 'RoomID' in os.environ:
         res['room'] = (os.environ['RoomID'])
     else:
-        res['room'] = (raw_input('Enter the ID of the Room which you wish to enter:'))
+        res['room'] = (raw_input('Enter the ID of the room you wish to enter: '))
                    
     if 'HostSite' in os.environ:
         res['host'] = (os.environ['HostSite'])
     else:
         import json
         hosts = json.loads(open('hosts.json').read())
-        print "Welcome Bot Host Site Options"
         for i in range(1, len(hosts)+1):
             print i, hosts[str(i)]
-        print "What will be your Welcome Bot's host site?"
         try:
-            res['host'] = hosts[raw_input().strip()]
+            res['host'] = hosts[raw_input("Enter the host site for Prime: ").strip()]
 
         except KeyError:
             res['host'] = "invalid" 
