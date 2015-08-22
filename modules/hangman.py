1ad22ac10c1dc655f531863fcd62bf8758e0ddad
Rishav Kundu <rishav.kundu98@gmail.com>
2015-08-23 00:21:03 +0530

    Revert "Merge remote-tracking branch 'origin/test'"
    This reverts commit 3b892d6fac005715adb82d5ecb4d76baab728410, reversing
    changes made to 0ea763aa21cdabb31e84eeefb7db28d7dff99cf6.

--- modules/hangman.py
+++ modules/hangman.py
@@ -9,9 +9,9 @@ the_end = False
 read_input = False
 
 def main(s, priv=False):
-    global buf
-    global _in
-    if 'start' in s:
+  global buf
+  global _in
+  if 'start' in s:
 	global the_end
 	the_end = True
 	buf = []
@@ -19,87 +19,157 @@ def main(s, priv=False):
 	sleep(2)
 	the_end = False
 	word = [urlopen('http://randomword.setgetgo.com/get.php').read().strip()]
-	n = thread.start_new_thread(game, tuple(word))
+	n = thread.start_new_thread(game, tuple(word)) 
 	print n
-    else:
+  else:
 	_in = s
-
-    while len(buf) == False:
+	
+	
+  while len(buf) == False:
 	pass
-    #read_input = False
-    print buf, _in
-    cp = buf[:]
-    buf = []
-    return '\n'.join(cp)
-
+  # read_input = False
+  print buf, _in
+  cp = buf[:]
+  buf = []
+  return '\n'.join(cp)
+  
+  
 def raw_input():
-    global _in
-    _in = '$$$'
-    #scary magic :6
-    while _in == '$$$':
+  global _in 
+  _in = '$$$'
+  #scary magic :6
+  while _in == '$$$':
 	if the_end == True:
-	    import sys
-	    sys.exit(0)
+	  import sys
+	  sys.exit(0)
 	pass
-    return _in
+  return _in
 
 def brint(s):
-    global buf
-    buf.append(s)
+  global buf
+  buf.append(s)
+  
+  
+  
+def game(word):
+  HANGMANPICS = ['''
 
-def displayBoard(missedLetters, correctLetters, secretWord):
-    blanks = '_' * len(secretWord)
+   +---+
+   |   |
+   |
+   |
+   |
+   |
+  =====''', '''
 
-    for i in range(len(secretWord)): # replace blanks with correctly guessed letters
-	if secretWord[i] in correctLetters:
-	    blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+   +---+
+   |   |
+   |   O
+   |
+   |
+   |
+  =====''', '''
 
-    brint(' '.join(blanks))
+   +---+
+   |   |
+   |   O
+   |   |
+   |
+   |
+  =====''', '''
 
-    if len(missedLetters) == 0:
-        brint('0/6 missed letters: (none)')
-    else:
-	brint(len(missedLetters) + '/6 missed letters: ' + ' '.join(missedLetters))
+   +---+
+   |   |
+   |   O
+   |  /|
+   |
+   |
+  =====''', '''
 
-def getGuess(alreadyGuessed):
-    while True:
-	brint('Guess a letter.')
-	guess = raw_input()
-	guess = guess.lower()
-	if len(guess) != 1:
-	    brint('Please enter a single letter.')
-	elif guess in alreadyGuessed:
-	    brint('You have already guessed that letter. Choose again.')
-	elif guess not in 'abcdefghijklmnopqrstuvwxyz':
-	    brint('Please enter a LETTER.')
-	else:
-	    return guess
+   +---+
+   |   |
+   |   O
+   |  /|\\
+   |
+   |
+  =====''', '''
 
-missedLetters = ''
-correctLetters = ''
-secretWord = word
-gameIsDone = False
+   +---+
+   |   |
+   |   O
+   |  /|\\
+   |  /
+   |
+  =====''', '''
 
-while not gameIsDone:
-    displayBoard(missedLetters, correctLetters, secretWord)
+   +---+
+   |   |
+   |   O
+   |  /|\\
+   |  / \\
+   |
+  =====''']
+  
+  def displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord):
+	 brint(HANGMANPICS[len(missedLetters)])
+	 brint('')
+	
+	 if len(missedLetters) == 0:
+		brint('Missed letters: None')
+	 else:
+		brint('Missed letters: ' + ' '.join(missedLetters))
 
-    print secretWord,'ALIBABA AND THE FORTY CHORS'
-	guess = getGuess(missedLetters + correctLetters)
-	if guess in secretWord:
-	    correctLetters = correctLetters + guess
+	 blanks = '_' * len(secretWord)
 
-	    foundAllLetters = True
-		for i in range(len(secretWord)):
-		    if secretWord[i] not in correctLetters:
-			foundAllLetters = False
-			break
-		if foundAllLetters:
-		    brint('You win! The word was "' + secretWord + '". You missed '+str(len(missedLetters))+' and guessed '+str(len(correctLetters))+' correct.')
-		    gameIsDone = True
-	else:
-	    missedLetters = missedLetters + guess
+	 for i in range(len(secretWord)): # replace blanks with correctly guessed letters
+		 if secretWord[i] in correctLetters:
+			 blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
 
-	    if len(missedLetters) == 6:
-		displayBoard(missedLetters, correctLetters, secretWord)
-		brint('You lose. The word was "' + secretWord + '". You missed '+str(len(missedLetters))+' and guessed '+str(len(correctLetters))+' correct.')
-		gameIsDone = True
+	 brint(' '.join(blanks))
+
+  def getGuess(alreadyGuessed):
+	 while True:
+		 brint('Guess a letter.')
+		 guess = raw_input()
+		 guess = guess.lower()
+		 if len(guess) != 1:
+			 brint('Please enter a single letter.')
+		 elif guess in alreadyGuessed:
+			 brint('You have already guessed that letter. Choose again.')
+		 elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+			 brint('Please enter a LETTER.')
+		 else:
+			 return guess
+
+
+  brint('H A N G M A N')
+  missedLetters = ''
+  correctLetters = ''
+  secretWord = word
+  gameIsDone = False
+
+  while not gameIsDone:
+	 displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord)
+	 
+	 print secretWord,'ALIBABA AND THE FORTY CHORS'
+	 guess = getGuess(missedLetters + correctLetters)
+	 if guess in secretWord:
+		 correctLetters = correctLetters + guess
+		
+		 foundAllLetters = True
+		 for i in range(len(secretWord)):
+			 if secretWord[i] not in correctLetters:
+				 foundAllLetters = False
+				 break
+		 if foundAllLetters:
+			 brint('You win! The word was "' + secretWord + '". You missed '+str(len(missedLetters))+' and guessed '+str(len(correctLetters))+' correct.')
+			 gameIsDone = True
+	 else:
+		 missedLetters = missedLetters + guess
+
+
+		 if len(missedLetters) == len(HANGMANPICS) - 1:
+			 displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord)
+			 brint('You lose. The word was "' + secretWord + '". You missed '+str(len(missedLetters))+' and guessed '+str(len(correctLetters))+' correct.')
+			 gameIsDone = True
+	
