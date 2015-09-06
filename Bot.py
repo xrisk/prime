f12a9cb5b4c42fccefd0662ac460b2223fb04369
Rishav Kundu <rishav.kundu98@gmail.com>
2015-09-06 22:53:20 +0530

    code refractoring

--- Bot.py
+++ Bot.py
@@ -1,63 +1,84 @@
+"""This module contains the `Bot` class, and `init_bot` utility method."""
+
+import ChatExchange.chatexchange.browser
 import ChatExchange.chatexchange.client
 import ChatExchange.chatexchange.events
-import ChatExchange.chatexchange.browser
-from random import choice
+import random
+
 
 class Bot:
-    def __init__(self, client, bot, room):
-        self.client = client
-        self.room = room
-        self.bot = bot
-        self.paused = False
-        
-    def pause(self):
-        self.paused = True
-        
-    def unpause(self):
-        self.paused = False
-        
-    def is_paused(self):
-        return self.paused
-    
-    def message(self, text):
-        if not self.paused:
-            self.room.send_message(text)
-            
-    def greet(self, user):
-        programming_languages = ["java", "python", "html", "c++", "bash", "javascript", "c", "vbscript", "php", "objective-c"]
-        language = choice(programming_languages)
-        username = '@'+user.replace(' ','')+'!'
-        if language == "java":
-            self.message("System.out.println(\"Hello, "+username+"\");")
-        elif language == "python":
-            self.message("print \"Hello, "+username+"\"")
-        elif language == "html":
-            self.message("<p>Hello, "+username+"</p>")
-        elif language == "c++":
-            self.message("std::cout << \"Hello, "+username+"\";")
-        elif language == "bash":
-            self.message("echo Hello, "+username)
-        elif language == "javascript":
-            self.message("document.write('Hello, "+username+"');")
-        elif language == "c":
-            self.message("puts(\"Hello, "+username+"\");")
-        elif language == "vbscript":
-            self.message("WScript.Echo \"Hello, "+username+"\"")
-        elif language == "php":
-            self.message("<?php echo 'Hello, "+username+"'; ?>")
-        elif language == "objective-c":
-            self.message("NSLog(@\"Hello, "+username+"\");")
-            
-    def start(self, callback):
-        self.room.watch(callback)
+
+	"""
+	A high level wrapper for the chatbot.
+
+	Notable methods included are `message` and `greet`.
+	"""
+
+	def __init__(self, client, room):
+		"""
+		Constructor for the `Bot` class.
+
+		@client=instance of `ChatExchange.chatexchange.client`
+		@room=room ID to join
+		"""
+		self.bot = self.client.get_me()
+		self.client = client
+		self.paused = False
+		self.room = self.client.get_room(room)
+		self.room.join()
+
+	def pause(self):
+		"""Pause the bot, has no effect if already paused."""
+		self.paused = True
+
+	def unpause(self):
+		"""Unpause the bot, has no effect if not paused."""
+		self.paused = False
+
+	def is_paused(self):
+		"""Return the pause state of the bot."""
+		return self.paused
+
+	def message(self, text):
+		"""Send a message to the current room. Ignore if paused."""
+		if not self.paused:
+			self.room.send_message(text)
+
+	def greet(self, user):
+		"""Greet a user."""
+		programming_languages = ["java", "python", "html", "c++", "bash",
+			"javascript", "c", "vbscript", "php", "objective-c"]
+		language = random.choice(programming_languages)
+		username = '@' + user.replace(' ', '') + '!'
+		if language == "java":
+			self.message("System.out.println(\"Hello, " + username + "\");")
+		elif language == "python":
+			self.message("print \"Hello, " + username + "\"")
+		elif language == "html":
+			self.message("<p>Hello, " + username + "</p>")
+		elif language == "c++":
+			self.message("std::cout << \"Hello, " + username + "\";")
+		elif language == "bash":
+			self.message("echo Hello, " + username)
+		elif language == "javascript":
+			self.message("document.write('Hello, " + username + "');")
+		elif language == "c":
+			self.message("puts(\"Hello, " + username + "\");")
+		elif language == "vbscript":
+			self.message("WScript.Echo \"Hello, " + username + "\"")
+		elif language == "php":
+			self.message("<?php echo 'Hello, " + username + "'; ?>")
+		elif language == "objective-c":
+			self.message("NSLog(@\"Hello, " + username + "\");")
+
+	def start(self, callback):
+		"""Start watching the room for events, sending them to `callback`."""
+		self.room.watch(callback)
+
 
 def init_bot(cred):
-    
-    client = ChatExchange.chatexchange.client.Client(str(cred['host'].encode('utf-8'))[5:])
-    client.login(cred['user'], cred['pass'])
-    bot = client.get_me()
-    room = client.get_room(int(cred['room']))
-    room.join()
-    return Bot(client, bot, room)
-            
-        
+	"""Utility method to generate a `Bot` object from a valid dictionary."""
+	client = ChatExchange.chatexchange.client.Client(
+		str(cred['host'].encode('utf-8'))[5:])
+	client.login(cred['user'], cred['pass'])
+	return Bot(client, int(cred['room']))
--- prime.py
+++ prime.py
@@ -1,109 +1,148 @@
-from Bot import Bot, init_bot
-import time
+"""
+prime.
+
+A chatbot which can be run in the rooms of stackoverflow.com
+This file contains the `main` function which launches the bot.
+Also contained are auxillary methods for the dynamic loading of commands.
+
+"""
+
+import Bot
 import ChatExchange.chatexchange.events
-import thread
-import os
-import sys
 import credentials
+import imp
+import json
+import os
+import shelve
+import sys
+import thread
+import time
 
 bot = None
 modules = None
 privileged = []
 
 
-def open(file,mode='r',buffering=-1):
-    import __builtin__    #use import builtins for Python 3.x
-    import os.path
-    filePath = os.path.join(os.path.dirname(__file__),file)
-    return __builtin__.open(filePath,mode,buffering)
-  
-  
+def open(file, mode='r', buffering=-1):
+	"""Hacky solution to resolve relative path imports. NOT for client use."""
+	import __builtin__
+	import os.path
+	file_path = os.path.join(os.path.dirname(__file__), file)
+	return __builtin__.open(file_path, mode, buffering)
+
+
 def init_modules():
-    import json, imp, os
-    global modules
-    try:
-        cur_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'modules')
-        modules = json.loads(open('modules.json').read())
-        for i in modules:
-            s = imp.load_source(i[2:], os.path.join(cur_path, modules[i]))
-            exec('import '+i[2:], globals())
-    except IOError, e:
-        print e
-    except ValueError, e:
-    	print e
-    
+	"""
+	Dynamically loads modules by reading the `modules.json` file.
+
+	Uses `inp` to parse .py files specified by `modules.json`.
+	Modules are then made available to the global namespace
+	using `import` in conjunction with `exec`.
+	"""
+	global modules
+	try:
+		cur_path = os.path.join(
+			os.path.dirname(os.path.abspath(__file__)), 'modules')
+		modules = json.loads(open('modules.json').read())
+		for i in modules:
+			imp.load_source(i[2:], os.path.join(cur_path, modules[i]))
+			exec('import ' + i[2:], globals())
+	except IOError, e:
+		print e
+	except ValueError, e:
+		print e
+
+
 def init_privileged():
-    import shelve
-    global privileged
-    f = shelve.open('privileged-users.db')
-    for i in f:
-        privileged.add(i)
-    i.close()
+	"""
+	Populate the `privileged` list.
+
+	Previously pickled User IDs are read from the privileged-users.db file.
+	"""
+	global privileged
+	f = shelve.open('privileged-users.db')
+	for i in f:
+		privileged.add(i)
+	i.close()
 
 
 def main():
-   
-    global bot
-    global modules
-    init_modules()
-                 
-    while True:
-        creds = credentials.get()
-        print creds
-        if credentials.validate(creds) == True: break
+	"""
+	An interactive function that starts the bot.
 
-    bot = init_bot(creds)
-    bot.start(on_event)
-    print 'PRIME HAS STARTED'
-    bot.message("I'm alive :)")
-    while True:
-      if 'no-input' not in sys.argv:  
-          text = raw_input(">> ")
-          if text == "die":
-              print 'PRIME HAS STOPPED RUNNING'
-              bot.message("I'm dead :(")
-              time.sleep(0.4)
-              break
-          else:
-              bot.message(text)
+	Initialises modules, populates `privileged`, and launches the bot.
+	Blocks indefinitely till the `die` command is supplied at sys.stdin`
+	"""
+	global bot
+	global modules
+	init_modules()
 
-    os._exit(6)
+	while True:
+		creds = credentials.get()
+		print creds
+		if credentials.validate(creds):
+			break
+
+	bot = Bot.init_bot(creds)
+	bot.start(on_event)
+	print 'PRIME HAS STARTED'
+	bot.message("I'm alive :)")
+	while True:
+		if 'no-input' not in sys.argv:
+			text = raw_input(">> ")
+			if text == "die":
+				print 'PRIME HAS STOPPED RUNNING'
+				bot.message("I'm dead :(")
+				time.sleep(0.4)
+			break
+		else:
+			bot.message(text)
+
+	os._exit(6)
 
 
 def on_event(event, client):
-   
-    if isinstance(event, ChatExchange.chatexchange.events.UserEntered):
-        on_enter(event)
-    elif isinstance(event, ChatExchange.chatexchange.events.MessagePosted):
-        on_command(bot, event)
+	"""Receive events fired by the bot and bubble them to appropriate handlers."""
+	if isinstance(event, ChatExchange.chatexchange.events.UserEntered):
+		on_enter(event)
+	elif isinstance(event, ChatExchange.chatexchange.events.MessagePosted):
+		on_command(bot, event)
 
 
 def on_enter(event):
-    print "User Entered"
-    if event.user.id == bot.bot.id or event.user.reputation < 20:
-        pass
-    else:
-        bot.greet(event.user.name)
-
+	"""The handler for when a user enters the chatroom being watched."""
+	print "User Entered"
+	if event.user.id == bot.bot.id or event.user.reputation < 20:
+		pass
+	else:
+		bot.greet(event.user.name)
 
 
 def on_command(bot, event):
-    text = event.content;
-    tokens = text.split()
-    if len(tokens) >= 1:
-        command = tokens[0]
-        if command in modules:
-            command = tokens[0][2:]
-            func = eval(command+'.main')
-            if event.user.id in privileged:
-                thread.start_new_thread(threaded_command, tuple([bot, func, event, True]))
-            else:
-                thread.start_new_thread(threaded_command, tuple([bot, func, event, False]))
+	"""The handler for when a message is posted."""
+	text = event.content
+	tokens = text.split()
+	if len(tokens) >= 1:
+		command = tokens[0]
+		if command in modules:
+			command = tokens[0][2:]
+			func = eval(command + '.main')
+			if event.user.id in privileged:
+				thread.start_new_thread(threaded_command, tuple([bot, func, event, True]))
+			else:
+				thread.start_new_thread(threaded_command, tuple([bot, func, event, False]))
+
 
 def threaded_command(bot, func, event, priv):
-    text=event.content
-    string = text[text.find(' '):].strip()
-    event.message.reply(func(string, priv))
+	"""
+	Execute the funtion object `func` and blocks till completion.
+
+	`func` is determined by `on_command`.
+	The function replies to the message with the string returned by `func`.
+	"""
+	text = event.content
+	string = text[text.find(' '):].strip()
+	event.message.reply(func(string, priv))
 
 if __name__ == '__main__':
-    main()
+	main()
